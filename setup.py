7feb229b78c76f8d236017941bf82e9203ddeb43
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2026-09-21 23:57:30 +0530

    repo name updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
+REPO_NAME = "Kidney-Disease-Classification"
 AUTHOR_USER_NAME = "Ravi-Katiyar04"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "drxravikatiyar0000@gmail.com"
